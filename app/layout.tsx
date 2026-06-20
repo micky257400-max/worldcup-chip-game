9519f024cea0a1856dcbac0ca67bf7afe8411ad9
Wruyne <wruyne@WruynedeMacBook-Air.local>
2026-06-20 11:53:09 +0800

    Set root layout edge runtime

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+export const runtime = "edge";
+
 export const metadata: Metadata = {
   title: "世界杯朋友虚拟筹码竞猜",
   description: "朋友房间、虚拟筹码、奖池制竞猜"
