e5046791683a658f2c99ba9e2958299b2eee794a
Wruyne <wruyne@WruynedeMacBook-Air.local>
2026-06-20 11:58:15 +0800

    Make Cloudflare edge runtime explicit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-export const runtime = "edge";
+export const runtime = 'edge';
 
 export const metadata: Metadata = {
   title: "世界杯朋友虚拟筹码竞猜",
